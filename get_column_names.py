fbd6dfb251e58c20fb743f3619641c808ef14b09
Abduvaliyevakumushxon <shodmonovd1985@gmail.com>
2024-02-04 19:57:07 +0500

    3-vazifa tugadi

--- get_column_names.py
+++ get_column_names.py
@@ -1,4 +1,5 @@
 #Define function,Get coloumn names from a csv file
+import csv
 def get_column_names(data):
     """ 
     Get column names from a csv file
@@ -7,6 +8,9 @@ def get_column_names(data):
     Returns:
         column_names: list of column names
     """
-    return
+    f=open("data.csv")
+    data=csv.reader(f)
+    return len(list(data)[0])
+print(get_column_names('data.csv'))
     
 # Read the csv file
